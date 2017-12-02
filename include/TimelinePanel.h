69a8ed7c22c83230695cb0ec1d7e1f97e33a62e2
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2017-12-01 20:42:45 -0800

    Reorder levels of protection within TimelinePanel class

--- include/TimelinePanel.h
+++ include/TimelinePanel.h
@@ -22,6 +22,39 @@
 namespace HOWL {
 	// Timeline panel
 	class EXPORT TimelinePanel: public wxHVScrolledWindow {
+		private:
+			wxPanel *m_parent;
+			wxBoxSizer *sizer;
+			int rowsize;
+			int colsize;
+			int headersize;
+			int labelsize;
+			std::map<int, KeyframeSet *> visible_layers;
+			bool headerclicked = false;
+			Project *activeProject = NULL;
+			wxWindowID eventTarget = NULL;
+
+			double colsPerBeat();
+			int measureFromCol(int col);
+			int beatFromCol(int col);
+			int divFromCol(int col);
+			int tickFromCol(int col);
+			int playhead_in_pixels();
+			wxPoint offset_in_pixels();
+			wxPoint mousepos_to_buttons(wxPoint mousepos);
+			wxPoint index_to_screenpos(wxPoint index);
+		
+		protected:
+			wxCoord OnGetRowHeight(size_t row) const;
+			wxCoord OnGetColumnWidth(size_t column) const;
+			void render_row(wxDC &canvas, std::string rowname, KeyframeSet *keyframes, wxRect bounding_box);
+			void render_header_segment(wxDC &canvas, int col, int xpos);
+			void render_selection(wxDC &canvas, Selection sel);
+			void render_header(wxDC &canvas);
+			void render_playhead(wxDC &canvas);
+			int playhead;
+			int ticksPerCol;
+
 		public:
 			TimelinePanel(wxPanel *parent, wxWindowID window_id, Project *project, wxWindowID eventTarget);
 			~TimelinePanel();
@@ -42,39 +75,6 @@ namespace HOWL {
 			int getPlayhead();
 			// void RefreshDisplay();
 			// void ChangeNoteColor(wxCommandEvent &event);
-		
-		protected:
-			wxCoord OnGetRowHeight(size_t row) const;
-			wxCoord OnGetColumnWidth(size_t column) const;
-			void render_row(wxDC &canvas, std::string rowname, KeyframeSet *keyframes, wxRect bounding_box);
-			void render_header_segment(wxDC &canvas, int col, int xpos);
-			void render_selection(wxDC &canvas, Selection sel);
-			void render_header(wxDC &canvas);
-			void render_playhead(wxDC &canvas);
-			int playhead;
-			int ticksPerCol;
-		
-		private:
-			wxPanel *m_parent;
-			wxBoxSizer *sizer;
-			int rowsize;
-			int colsize;
-			int headersize;
-			int labelsize;
-			std::map<int, KeyframeSet *> visible_layers;
-			bool headerclicked = false;
-			Project *activeProject = NULL;
-			wxWindowID eventTarget = NULL;
-		
-			double colsPerBeat();
-			int measureFromCol(int col);
-			int beatFromCol(int col);
-			int divFromCol(int col);
-			int tickFromCol(int col);
-			int playhead_in_pixels();
-			wxPoint offset_in_pixels();
-			wxPoint mousepos_to_buttons(wxPoint mousepos);
-			wxPoint index_to_screenpos(wxPoint index);
 		
 			wxDECLARE_EVENT_TABLE();
 	};
